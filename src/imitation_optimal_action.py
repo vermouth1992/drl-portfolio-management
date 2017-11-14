d5a15f22d12b21110396b4dcefd904e712404768
vermouth1992 <czhangseu@gmail.com>
2017-11-14 15:55:25 -0800

    current model yields 45% testing accuracy

--- src/imitation_optimal_action.py
+++ src/imitation_optimal_action.py
@@ -5,7 +5,7 @@ from utils.data import read_stock_history, create_optimal_imitation_dataset, cre
 
 import numpy as np
 
-from keras.layers import Dense, Activation, BatchNormalization, Dropout, Conv1D, Flatten, MaxPooling1D
+from keras.layers import Dense, Activation, BatchNormalization, Dropout, Conv1D, Flatten, MaxPooling1D, Conv2D
 from keras.models import Sequential
 from keras.optimizers import Adam
 from keras.utils import np_utils
@@ -17,6 +17,12 @@ target_stocks = ['AAPL', 'CMCSA', 'REGN']
 target_history = np.empty(shape=(len(target_stocks), history.shape[1], history.shape[2]))
 for i, stock in enumerate(target_stocks):
     target_history[i] = history[abbreviation.index(stock), :, :]
+
+# test on 3 never seen stocks
+test_stocks = ['GOOGL', 'DISH', 'ILMN']
+test_history = np.empty(shape=(len(test_stocks), history.shape[1], history.shape[2]))
+for i, stock in enumerate(test_stocks):
+    test_history[i] = history[abbreviation.index(stock), :, :]
 
 
 def create_network_given_future(nb_classes):
@@ -54,10 +60,17 @@ def train_optimal_action_given_future_obs(target_history, target_stocks):
 
 def create_network_give_past(nb_classes, window_length):
     model = Sequential()
-    model.add(Conv1D(16, 3, input_shape=(window_length, nb_classes), activation='relu'))
-    model.add(MaxPooling1D())
+    model.add(Conv2D(filters=32, kernel_size=(1, 3), input_shape=(nb_classes, window_length, 1),
+                     activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Conv2D(filters=32, kernel_size=(1, window_length - 2), input_shape=(nb_classes, window_length - 2, 1),
+                     activation='relu'))
+    model.add(Dropout(0.5))
     model.add(Flatten(input_shape=(window_length, nb_classes)))
-    model.add(Dense(16, activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
     model.add(Dense(nb_classes, activation='softmax'))
     model.compile(loss='categorical_crossentropy',
                   optimizer=Adam(lr=1e-3),
@@ -67,15 +80,25 @@ def create_network_give_past(nb_classes, window_length):
 
 def train_optimal_action_given_history_obs(target_history, target_stocks, window_length):
     nb_classes = len(target_stocks) + 1
-    (X_train, y_train), (X_test, y_test) = create_imitation_dataset(target_history, window_length)
+    (X_train, y_train), (X_validation, y_validation) = create_imitation_dataset(target_history, window_length)
     Y_train = np_utils.to_categorical(y_train, nb_classes)
-    Y_test = np_utils.to_categorical(y_test, nb_classes)
+    Y_validation = np_utils.to_categorical(y_validation, nb_classes)
     model = create_network_give_past(nb_classes, window_length)
-    X_train = np.transpose(X_train, (0, 2, 1))
-    X_test = np.transpose(X_test, (0, 2, 1))
-    model.fit(X_train, Y_train, batch_size=128, epochs=100, validation_data=(X_test, Y_test), shuffle=True)
+    X_train = np.expand_dims(X_train, axis=-1)
+    X_validation = np.expand_dims(X_validation, axis=-1)
+    continue_train = True
+    while continue_train:
+        model.fit(X_train, Y_train, batch_size=128, epochs=100, validation_data=(X_validation, Y_validation),
+                  shuffle=True)
+        continue_train = input("True to continue train, otherwise stop training...\n")
     return model
 
+
 if __name__ == '__main__':
-    optimal_given_future = train_optimal_action_given_future_obs(target_history, target_stocks)
-    optimal_given_history = train_optimal_action_given_history_obs(target_history, target_stocks, 7)
+    # optimal_given_future = train_optimal_action_given_future_obs(target_history, target_stocks)
+    optimal_given_history_model = train_optimal_action_given_history_obs(target_history, target_stocks, 3)
+    (X_test, y_test), (_, _) = create_imitation_dataset(test_history, 3)
+    Y_test = np_utils.to_categorical(y_test, 4)
+    X_test = np.expand_dims(X_test, axis=-1)
+    loss, acc = optimal_given_history_model.evaluate(X_test, Y_test)
+    print('Testing result: loss - {}, accuracy - {}'.format(loss, acc))
